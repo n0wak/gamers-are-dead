d6fe04c9575d21c8c1528eae05a0bf9fa294ae84
n0wak <mnowak+git@gmail.com>
2014-10-28 10:19:48 +0000

    Initial commit

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 all:
-	DISPLAY=:0 sudo google-chrome --pack-extension=Source --pack-extension-key=CloudToButt.pem --user-data-dir=/tmp/foooo
+	DISPLAY=:0 sudo google-chrome --pack-extension=Source --pack-extension-key=GamersAreDead.pem --user-data-dir=/tmp/foooo
 	sudo chown hank:hank Source.crx
-	mv Source.crx CloudToButt.crx
+	mv Source.crx GamersAreDead.crx
--- Source/content_script.js
+++ Source/content_script.js
@@ -30,35 +30,12 @@ function walk(node)
 }
 
 function handleText(textNode) {
-	var v = textNode.nodeValue;
-
-  // Deal with the easy case
-  v = v.replace(/\b(T|t)he (C|c)loud/g, function(match, p1, p2, offset, string) {
-    // t - 7 = m
-    // c - 1 = b
-    m = String.fromCharCode(p1.charCodeAt(0) - 7);
-    b = String.fromCharCode(p2.charCodeAt(0) - 1);
-    return m + "y " + b + "utt";
-  });
-
-  // Deal with private clouds
-  v = v.replace(/\b(P|p)rivate (C|c)loud/g, function(match, p1, p2, offset, string) {
-    // c - 1 = b
-    b = String.fromCharCode(p2.charCodeAt(0) - 1);
-    return b + "utt";
-  });
-  // Get the corner cases
-  if(v.match(/cloud/i)) {
-    // If we're not talking about weather
-    if(v.match(/PaaS|SaaS|IaaS|computing|data|storage|cluster|distributed|server|hosting|provider|grid|enterprise|provision|apps|hardware|software|/i)) {
-      v = v.replace(/(C|c)loud/gi, function(match, p1, offset, string) {
-        // c - 1 = b
-        b = String.fromCharCode(p1.charCodeAt(0) - 1);
-        return b + "utt";
-      });
-    }
-  }
-	textNode.nodeValue = v;
+    var v = textNode.nodeValue;
+    var hobbyAsIdentity = ["Televisioner", "Movier", "Sportser", "Booker", "Musicer", "Drawer", "Fooder", "Exerciser", "Shopper", "Relaxer", "Crafter", "Dancer", "Theatreer", "Drinker", "Genealogier", "Knitter", "Websurfer", "Trainspotter"];
+    var thisHobby = hobbyAsIdenty[Math.floor(Math.random()*hobbyAsIdenty.length)];
+    v = v.replace(/\bGamer\b/g, thisHobby);
+    v = v.replace(/\bgamer\b/g, thisHobby.toLowerCase());
+    textNode.nodeValue = v;
 }
 
 
